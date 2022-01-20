7090f385c2d929925409b7a64bc1fe0f6252de79
Sanjay Kumar <sanjaykumar73189@gmail.com>
2022-01-20 22:47:14 +0530

    created back, forward, reload, home button and text area

--- main.py
+++ main.py
@@ -7,37 +7,38 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.browser= QWebEngineView()
-        self.browser.setUrl(QUrl('https://google.com'))
+        self.browser.setUrl(QUrl('https://google.com'))   #getting home page of browser
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
         #nav bar
-        navbar = QToolBar()
+        navbar = QToolBar()       #navigation bar
         self.addToolBar(navbar)
 
-        back_btn = QAction('Back', self)
+        back_btn = QAction('Back', self)   #back button in browser
         back_btn.triggered.connect(self.browser.back)
         navbar.addAction(back_btn)
-        forwrd_btn = QAction('Forward', self)
+
+        forwrd_btn = QAction('Forward', self)   #forwrd button in browser
         forwrd_btn.triggered.connect(self.browser.forward)
         navbar.addAction(forwrd_btn)
 
-        home = QAction('Home', self)
+        home = QAction('Home', self)     #home button in browser
         home.triggered.connect(self.navigate_home)
         navbar.addAction(home)
 
-        reload= QAction('Reload', self)
+        reload= QAction('Reload', self)   #reload button in browser
         reload.triggered.connect(self.browser.reload)
         navbar.addAction(reload)
 
-        self.url_bar = QLineEdit()
+        self.url_bar = QLineEdit()     #adding text area for entering url
         self.url_bar.returnPressed.connect(self.navigate_to_Url)
         navbar.addWidget(self.url_bar)
 
-        self.browser.urlChanged.connect(self.update_url)
+        self.browser.urlChanged.connect(self.update_url)   #noting url change in text area
 
     def navigate_home(self):
-        self.browser.setUrl(QUrl('https://google.com'))
+        self.browser.setUrl(QUrl('https://google.com'))   #setting home url
 
     def navigate_to_Url(self):
         url = self.url_bar.text()
